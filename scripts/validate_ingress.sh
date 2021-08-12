dd475200878bdcf0c494a9da5694ab1529da3f23
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-08-11 23:09:23 -0500

    [ncheneweth] add back istio-tools

--- scripts/validate_ingress.sh
+++ scripts/validate_ingress.sh
@@ -7,10 +7,22 @@ export CROSS_ACCOUNT_DOMAIN=$(cat $CLUSTER.json | jq -r '.cross_account_domain')
 
 # deploy and test httpbin on same account domain
 bash scripts/deploy_httpbin.sh ${CLUSTER} ${SAME_ACCOUNT_DOMAIN}
-bats test/validate_twdps_digital.bats
+
+# this is a complete hack - need to sort out params to bats tests
+if [[ $CLUSTER == "sandbox" ]]; then
+  bats test/validate_sandbox_twdps_digital.bats
+elseif [[ $CLUSTER == "preview" ]]; then
+  bats test/validate_preview_twdps_digital.bats
+fi
 
 # deploy and test httpbin on cross account domain
 bash scripts/deploy_httpbin.sh ${CLUSTER} ${CROSS_ACCOUNT_DOMAIN}
-bats test/validate_twdps_io.bats
+
+# this is a complete hack - need to sort out params to bats tests
+if [[ $CLUSTER == "sandbox" ]]; then
+  bats test/validate_sandbox_twdps_io.bats
+elseif [[ $CLUSTER == "preview" ]]; then
+  bats test/validate_preview_twdps_io.bats
+fi
 
 kubectl delete -f httpbin.$CLUSTER.$CROSS_ACCOUNT_DOMAIN.yaml
