3fe564fc1194d98960ee3c7aa51259d7e9490897
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-08-11 23:34:24 -0500

    [ncheneweth] add back istio-tools

--- scripts/validate_ingress.sh
+++ scripts/validate_ingress.sh
@@ -7,21 +7,25 @@ export CROSS_ACCOUNT_DOMAIN=$(cat $CLUSTER.json | jq -r '.cross_account_domain')
 
 # deploy and test httpbin on same account domain
 bash scripts/deploy_httpbin.sh ${CLUSTER} ${SAME_ACCOUNT_DOMAIN}
+sleep 10
 
 # this is a complete hack - need to sort out params to bats tests
 if [[ $CLUSTER == "sandbox" ]]; then
   bats test/validate_sandbox_twdps_digital.bats
-elif [[ $CLUSTER == "preview" ]]; then
+fi
+if [[ $CLUSTER == "preview" ]]; then
   bats test/validate_preview_twdps_digital.bats
 fi
 
 # deploy and test httpbin on cross account domain
 bash scripts/deploy_httpbin.sh ${CLUSTER} ${CROSS_ACCOUNT_DOMAIN}
+sleep 10
 
 # this is a complete hack - need to sort out params to bats tests
 if [[ $CLUSTER == "sandbox" ]]; then
   bats test/validate_sandbox_twdps_io.bats
-elif [[ $CLUSTER == "preview" ]]; then
+fi
+if [[ $CLUSTER == "preview" ]]; then
   bats test/validate_preview_twdps_io.bats
 fi
 
